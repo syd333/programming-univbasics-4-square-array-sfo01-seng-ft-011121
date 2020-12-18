d541854cb6671c01b0a9cefffe824ce9c35fcba2
sydney miranda <sydmiranda@icloud.com>
2020-12-18 15:15:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   new_array = []
   array.length.times do |index|
-    new_array.push(array[index] **)
+    new_array.push(array[index] ** array[index])
   end
   new_array
 end
