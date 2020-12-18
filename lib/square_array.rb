75ecf03c906084adcafc58984ee6336adbc149da
sydney miranda <sydmiranda@icloud.com>
2020-12-18 15:21:29 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+  new_array = []
+  index = 0
+  while index < array.length
+    new_array.push(array[index] * array[index])
+    index +=1
+  end
+  new_array
 end
