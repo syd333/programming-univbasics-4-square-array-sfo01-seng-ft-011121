139e7f30490a913ec02105d7dc2a5494aec24615
sydney miranda <sydmiranda@icloud.com>
2020-12-18 15:10:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,6 +2,7 @@ def square_array(array)
   # your code here
   new_array = []
   while count < array.length |index| do
-    new_array.push(array[index])
+    new_array.push(array[index] * 1)
   end
+  new_array
 end
